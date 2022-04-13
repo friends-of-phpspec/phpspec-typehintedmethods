9d17a98c490b8afbeca7c797bea1965ae3b81b54
Alex Torchenko <torchenko@gmail.com>
2022-04-13 00:32:21 -0400

    Fix deprecation

--- src/Generator/TypeHintedMethodGenerator.php
+++ src/Generator/TypeHintedMethodGenerator.php
@@ -91,7 +91,7 @@ class TypeHintedMethodGenerator implements Generator
      */
     protected function getTemplate()
     {
-        return file_get_contents(__FILE__, null, null, __COMPILER_HALT_OFFSET__);
+        return file_get_contents(__FILE__, false, null, __COMPILER_HALT_OFFSET__);
     }
 }
 __halt_compiler();
